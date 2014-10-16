ed6a354bfea48956918725dfb4afa6619414b3f6
Richard Kwo <richardkwo@gmail.com>
2014-10-16 02:28:18 -0400

    ...

--- html/github.php
+++ html/github.php
@@ -1,9 +1,2 @@
 <?php
-
- // Prevent accidental XSS
- header("Content-type: text/plain"); 
-
- // Run the script - make sure that your SSH key is set with *no* password
- if ( $_POST['payload'] ) {
-  shell_exec("git pull"); 
-}
+echo `cd .. && git pull`;
